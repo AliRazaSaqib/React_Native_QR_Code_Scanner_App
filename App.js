de56a35792ca979f1884b31469705029c04525ec
Ali Raza <ali.raza@codeninjaconsulting.com>
2022-11-12 14:38:05 +0500

    initial commit

--- App.js
+++ App.js
@@ -1,18 +1,21 @@
-import React, { useState, useEffect } from 'react';
-import { Text, View, StyleSheet, Button } from 'react-native';
-import { BarCodeScanner } from 'expo-barcode-scanner';
+import React, { useState, useEffect } from "react";
+import { Text, View, StyleSheet, Button, ScrollView } from "react-native";
+import { BarCodeScanner } from "expo-barcode-scanner";
 
 export default function App() {
   const [hasPermission, setHasPermission] = useState(null);
   const [scanned, setScanned] = useState(false);
-  const [text, setText] = useState('Not yet scanned')
+  const [text, setText] = useState(
+    "Not yet scanned please scan first to see the results!"
+  );
+  const [batch, setBatch] = useState("BSBI batch 23");
 
   const askForCameraPermission = () => {
     (async () => {
       const { status } = await BarCodeScanner.requestPermissionsAsync();
-      setHasPermission(status === 'granted');
-    })()
-  }
+      setHasPermission(status === "granted");
+    })();
+  };
 
   // Request Camera Permission
   useEffect(() => {
@@ -22,8 +25,16 @@ export default function App() {
   // What happens when we scan the bar code
   const handleBarCodeScanned = ({ type, data }) => {
     setScanned(true);
-    setText(data)
-    console.log('Type: ' + type + '\nData: ' + data)
+    setText(data);
+    setBatch("");
+    // console.log("Type: " + type + "\nData: " + data);
+  };
+
+  //scan again function
+  const handleScanAgain = () => {
+    setScanned(false);
+    setText("Not yet scanned please scan first to see the results!");
+    setBatch("BSBI batch 23");
   };
 
   // Check permissions and return the screens
@@ -31,14 +42,23 @@ export default function App() {
     return (
       <View style={styles.container}>
         <Text>Requesting for camera permission</Text>
-      </View>)
+      </View>
+    );
   }
   if (hasPermission === false) {
     return (
       <View style={styles.container}>
-        <Text style={{ margin: 10 }}>No access to camera</Text>
-        <Button title={'Allow Camera'} onPress={() => askForCameraPermission()} />
-      </View>)
+        <Text style={{ margin: 10, textAlign: "center" }}>
+          No access to camera, if you want to scan your QR-Codes first allow
+          your camera access
+        </Text>
+        <Button
+          title={"Allow Camera"}
+          onPress={() => askForCameraPermission()}
+          color="#F4717F"
+        />
+      </View>
+    );
   }
 
   // Return the View
@@ -47,11 +67,23 @@ export default function App() {
       <View style={styles.barcodebox}>
         <BarCodeScanner
           onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
-          style={{ height: 400, width: 400 }} />
+          style={{ height: 400, width: 400 }}
+        />
       </View>
-      <Text style={styles.maintext}>{text}</Text>
+      <ScrollView style={{ height: 40 }}>
+        <Text style={styles.maintext}>{text}</Text>
+        <Text style={{ textAlign: "center", fontSize: 24, color: "#F4717F" }}>
+          {batch}
+        </Text>
+      </ScrollView>
 
-      {scanned && <Button title={'Scan again?'} onPress={() => setScanned(false)} color='tomato' />}
+      {scanned && (
+        <Button
+          title={"Scan again"}
+          onPress={handleScanAgain}
+          color="#F4717F"
+        />
+      )}
     </View>
   );
 }
@@ -59,21 +91,23 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
   },
   maintext: {
-    fontSize: 16,
-    margin: 20,
+    fontSize: 14,
+    margin: 4,
+    padding: 12,
+    borderRadius: 12,
+    textAlign: "center",
+    overflow: "visible",
   },
   barcodebox: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: 300,
-    width: 300,
-    overflow: 'hidden',
-    borderRadius: 30,
-    backgroundColor: 'tomato'
-  }
+    alignItems: "center",
+    justifyContent: "center",
+    height: 280,
+    overflow: "hidden",
+    backgroundColor: "#F4717F",
+  },
 });
